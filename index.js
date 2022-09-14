94e174dbc3615f23d31cdf619dbbd3124debd395
Adrian Lopez <adrian.08lv@gmail.com>
2022-09-14 14:22:02 -0300

    Proyecto terminado, agregado array de ordenes y terminados ultimos estilos

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const sideMenu = document.querySelector("aside")
 const menuBtn = document.querySelector("#menu-btn")
 const closeBtn = document.querySelector("#close-btn")
+const themeToggle = document.querySelector(".theme-toggler")
+
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block'
@@ -8,4 +10,30 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none'
+})
+
+
+// change theme
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggle.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggle.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+
+
+// fill table with orders
+Orders.forEach(order => {
+    const tr = document.createElement('tr')
+    const trContent = `
+                <td>${order.productName}</td>
+                <td>${order.productNumber}</td>
+                <td>${order.paymentStatus}</td>
+                <td class="${order.shiping === 'Declined' ? 'danger' : 
+                order.shiping === 'Pending' ? 'warning' : 'primary'}">
+                ${order.shiping}</td>
+                <td class="primary"><a href="#">Details<a></td>
+    `
+    tr.innerHTML = trContent
+    document.querySelector('table tbody').appendChild(tr)
 })
---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shiping: 'Declined'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shiping: 'Pending'
+    },
+    {
+        productName: 'DJI Air 2s',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shiping: 'Delivered'
+    },
+    {
+        productName: 'Kizebge Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shiping: 'Delivered'
+    },
+]
